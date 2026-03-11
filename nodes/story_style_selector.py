cd1f3dd5a3d6d49b8d986722000d977800bc61e9
mickmumpitz <mumpicorp@gmail.com>
2026-03-11 11:37:27 +0100

    Add multi string nodes

---
+++ nodes/story_style_selector.py
@@ -0,0 +1,34 @@
+STORY_OPTIONS = ["Motorcycle vs. Insect", "A Knights Tale", "Cyberpunk"]
+STYLE_OPTIONS = ["Realistic", "Anime", "Stop Motion"]
+
+
+class StoryStyleSelector:
+    """Select a story and style by name, outputs their 1-based indices."""
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "story": (STORY_OPTIONS, {"default": STORY_OPTIONS[0]}),
+                "style": (STYLE_OPTIONS, {"default": STYLE_OPTIONS[0]}),
+            },
+        }
+
+    RETURN_TYPES = ("INT", "INT")
+    RETURN_NAMES = ("story_index", "style_index")
+    FUNCTION = "select"
+    CATEGORY = "Mickmumpitz/String Pack"
+
+    def select(self, story, style):
+        story_index = STORY_OPTIONS.index(story) + 1
+        style_index = STYLE_OPTIONS.index(style) + 1
+        return (story_index, style_index)
+
+
+NODE_CLASS_MAPPINGS = {
+    "StoryStyleSelector": StoryStyleSelector,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "StoryStyleSelector": "Story & Style Selector",
+}
